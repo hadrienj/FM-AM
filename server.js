1ac81fca90400cf04a6fe5403296610b2f257393
hadrienj <hadrienjean@gmail.com>
2017-10-28 15:39:33 +0200

    handle statics

--- server.js
+++ server.js
@@ -1,37 +1,44 @@
-var app = require('http').createServer(handler),
-  fs = require('fs'),
-  static = require('node-static');
+var app = require('http').createServer(handler);
+var fs = require('fs');
+var static = require('node-static');
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8000;
 
+// Create a node-static server instance to serve the './static' folder
+var staticFiles = new static.Server('./statics');
+
 // handle web server
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading client.html');
-    }
-    res.writeHead(200);
-    res.end(data);
-  });
+function handler (request, response) {
+  // fs.readFile(__dirname + '/index.html', function (err, data) {
+  //   if (err) {
+  //     console.log(err);
+  //     response.writeHead(500);
+  //     return response.end('Error loading client.html');
+  //   }
+  //   response.writeHead(200);
+  //   response.end(data);
+  // });
+  // serve static files
+  request.addListener('end', function () {
+      staticFiles.serve(request, response);
+      console.log('static');
+  }).resume();
 }
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
 
+// require('http').createServer(function (request, response) {
+//     request.addListener('end', function () {
+//         //
+//         // Serve files!
+//         //
+//         staticFiles.serve(request, response);
+//         console.log('Our app is running on http://localhost:' + port);
+//     }).resume();
+// }).listen(port);
 
-// Create a node-static server instance to serve the './static' folder
-var staticFiles = new static.Server('./statics');
 
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        staticFiles.serve(request, response);
-    }).resume();
-}).listen(8080);
--- statics/modulations.js
+++ statics/modulations.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
 var modulations = {
     play: false,
     column: 0,
